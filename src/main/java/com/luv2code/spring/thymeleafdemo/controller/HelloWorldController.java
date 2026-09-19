f3ac9717364a87b347d5d434b9eef3125a1b23e9
Muayad Haddad <74570275+muayadhaddad59@users.noreply.github.com>
2026-09-19 19:59:31 -0400

    feat: add fields and options

--- src/main/java/com/luv2code/spring/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/luv2code/spring/thymeleafdemo/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.luv2code.spring.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWorldController {
 
     // new a controller method to show initial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -38,7 +39,7 @@ public class HelloWorldController {
     }
 
 
-    @RequestMapping("/processFormVersionThree")
+    @GetMapping("/processFormVersionThree")
     public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
         // convert the data to all caps
         theName = theName.toUpperCase();
